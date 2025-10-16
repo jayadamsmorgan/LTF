27f815e0a7bedc2c2b5a58da0a27e61dd3501b4c
yproshin <y.proshin@akytec.rs>
2025-10-16 19:00:01 +0200

    New functions for ssh.lua: create_conenction, close connection

--- examples/ssh-example/tests/test.lua
+++ examples/ssh-example/tests/test.lua
@@ -2,9 +2,10 @@ local ltf = require("ltf")
 local ssh = ltf.ssh
 local vars = require("variables")
 
+-- Just connect and disconnect from local host with low level API
 ltf.test({
 	name = "SSH low level connect",
-	tags = { "tag1", "tag2" },
+	tags = { "tag1" },
 	body = function()
 		local res = ssh.low.lib_init()
 		print("lib init: ", res)
@@ -23,24 +24,22 @@ ltf.test({
 	end,
 })
 
+-- Connect and execute command via ssh on local host with ssh.lua API
 ltf.test({
-	name = "SSH low level connect",
-	tags = { "tag1", "tag2" },
+	name = "SSH connect and disconnect with high level API",
+	tags = { "tag2" },
 	body = function()
-		local res = ssh.low.lib_init()
-		print("lib init: ", res)
-		local session = ssh.low.session_init()
-		print("Session init: ", session)
-		local socket, err = ssh.low.socket_init(vars.host_ip, vars.ssh_port)
-		print("Socket init: ", socket, err)
-		res = session:handshake(socket)
-		print("Handshake res: ", res)
+		local conn1 = ssh.create_connection(vars.host_ip, vars.ssh_port, "yproshin", "CPE%FUZD")
 
-		res = session:disconnect("User disconnect")
-		print("Disconnect res: ", res)
-		res = session:free()
-		print("Sessio free res: ", res)
-		res = ssh.low.socket_free(socket)
-		print("Socket free res: ", res)
+		ltf.log_info("ip: ", conn1.ip)
+		ltf.log_info("port: ", conn1.port)
+		ltf.log_info("usr: ", conn1.usr)
+		ltf.log_info("pswd: ", conn1.pswd)
+		ltf.log_info("session: ", conn1.session)
+		ltf.log_info("socket: ", conn1.socket)
+
+		-- ssh.execute_cmd(conn1,"cmd", true, true)
+
+		ssh.close_connection(conn1)
 	end,
 })
--- lib/ltf/ssh.lua
+++ lib/ltf/ssh.lua
@@ -1,3 +1,5 @@
+-- Curent lib version working only in blocking  mode
+
 local ltf = require("ltf-main")
 local ts = require("ltf-ssh")
 
@@ -6,6 +8,83 @@ local M = {}
 -- For low level function usage in tests
 M.low = ts
 
-M.easy_connect = function() end
+-- Arguments: ip, port, usr, pswd,
+M.create_connection = function(ip, port, usr, pswd)
+	local res, err = ts.lib_init()
+	if res == nil then
+		error(err)
+	end
+
+	local session, err = ts.session_init()
+
+	if session == nil then
+		error(err)
+	end
+
+	local socket, err = ts.socket_init(ip, port)
+	if socket == nil then
+		error(err)
+	end
+
+	res, err = session:handshake(socket)
+	if res == nil then
+		error(err)
+	end
+
+	res, err = session:userauth_password(usr, pswd)
+	if res == nil then
+		error(err)
+	end
+	-- create connection struct
+	local connection = {
+		ip = ip,
+		port = port,
+		usr = usr,
+		pswd = pswd,
+		session = session,
+		socket = socket,
+	}
+
+	return connection
+end
+
+-- Arguments: ip, port, usr, pswd,
+M.close_connection = function(connection)
+	local res, err = connection.session:disconnect("User disconnect")
+	if res == nil then
+		ltf.log_error(err)
+	end
+
+	res, err = connection.session:free()
+	if res == nil then
+		ltf.log_error(err)
+	end
+
+	res, err = ts.socket_free(connection.socket)
+	if res == nil then
+		ltf.log_error(err)
+	end
+end
+
+-- Arguments: connection with stdout and stderr output
+M.execute_cmd = function(connection, cmd, stdout_b, stderr_b) end
+
+-- Arguments:
+M.open_shell = function(connection, cmd) end
+
+-- Arguments:
+M.shell_write = function(connection, cmd) end
+
+-- Arguments:
+M.shell_read_until = function(connection, cmd) end
+
+-- Arguments:
+M.close_shell = function(connection, cmd) end
+
+-- Arguments:
+M.send_file_scp = function(connection, cmd) end
+
+-- Arguments:
+M.recv_file_scp = function(connection, cmd) end
 
 return M
