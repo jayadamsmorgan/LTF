452f645774c743ace7f541a7d74e6d8e60498b22
Iaroslav Proshin <iaroslavproshin@gmail.com>
2026-01-11 21:57:47 +0100

    Fixes for not headless launch of webdriver  selftests

--- selftest/lib/webdriver_util.lua
+++ selftest/lib/webdriver_util.lua
@@ -6,21 +6,6 @@ local wd = ltf.webdriver
 --- @return wd_session
 M.setup = function()
 	local port = 9515
-	local proc_handle = wd.spawn_webdriver({
-		webdriver = "geckodriver",
-		port = port,
-	})
-	ltf.defer(function()
-		proc_handle:kill()
-	end)
-
-	-- Just making sure it runs:
-	ltf.sleep(5000)
-	local status = proc_handle:wait()
-	if status ~= nil then
-		local stderr = proc_handle:read("stderr", nil)
-		error("Unable to start webdriver: " .. stderr)
-	end
 
 	local session = wd.new_session({
 		port = port,
